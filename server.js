507035eff22cda7b97923e0215ce4afc03585401
tyler-Wiz <tylertooxclusive@gmail.com>
2023-10-02 09:13:57 +0100

    Quote API Challenge Project (JavaScript)

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+
+const { quotes } = require("./data");
+const { getRandomElement } = require("./utils");
+
+const PORT = process.env.PORT || 4001;
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log(`Now working at ${PORT}`);
+});
+
+app.get("/", (req, res, next) => {
+  res.send("welcome to the API");
+});
+
+app.get("/api/quotes/random", (req, res, next) => {
+  const randomQuote = getRandomElement(quotes);
+  res.send({ quote: randomQuote });
+});
+
+app.get("/api/quotes", (req, res, next) => {
+  const { quote, person } = req.query;
+  if (!person) {
+    return res.send({ quotes: quotes });
+  }
+  if (person !== "") {
+    const search = (text) =>
+      quotes.filter(({ person }) => person.includes(text));
+    const result = search(person);
+    res.send({ quotes: result });
+  }
+});
+
+app.post("/api/quotes", (req, res, next) => {
+  const { quote, person } = req.query;
+  if (quote === "" || person === "") {
+    return res.status(400).send("Doesn't Quote or person doesn't is not added");
+  }
+  quotes.push(req.query);
+  res.send({ quote: req.query });
+});
